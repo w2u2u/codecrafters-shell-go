d4c4364c45acb7041567b5c7c28854288ffafd89
Warunyu Jaimuang <warunyu.pon@gmail.com>
2024-05-27 18:34:59 +0700

    codecrafters submit [skip ci]

--- cmd/myshell/main.go
+++ cmd/myshell/main.go
@@ -7,12 +7,7 @@ import (
 )
 
 func main() {
-	// You can use print statements as follows for debugging, they'll be visible when running tests.
-	fmt.Println("Logs from your program will appear here!")
+	fmt.Fprint(os.Stdout, "$ ")
 
-	// Uncomment this block to pass the first stage
-	// fmt.Fprint(os.Stdout, "$ ")
-
-	// Wait for user input
 	bufio.NewReader(os.Stdin).ReadString('\n')
 }
